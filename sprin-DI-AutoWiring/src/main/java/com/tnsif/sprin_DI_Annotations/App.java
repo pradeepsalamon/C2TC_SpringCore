aa6dd23fc94800101721d36e401e64722774dacd
pradeepsalamon <pradeepsalamon14@gmail.com>
2025-11-04 18:24:55 +0530

    init

---
+++ sprin-DI-AutoWiring/src/main/java/com/tnsif/sprin_DI_Annotations/App.java
@@ -0,0 +1,10 @@
+package com.tnsif.sprin_DI_Annotations;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
